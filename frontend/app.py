704b5612c9098cb6bbe911a2cf8cb8ac9268a5e2
Deepak Kumar Sinha <dk21509@gmail.com>
2026-06-08 14:10:37 +0530

    add docker skelton

---
+++ frontend/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Enterprise RAG Platform",
+    layout="wide"
+)
+st.title("Enterprise RAG Platform")
+st.write("Frontend is running successfully.")
